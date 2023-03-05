59b1648728d3c9e6afc667658a6ec2dc75975b41
Bazjam <bonusystem@gmail.com>
2023-03-05 19:48:50 +0200

    map Home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev top tips", body: "lorem ipum...", author: "mario", id: 3 },
+  ]);
+
+  console.log(blogs);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map(e => (
+        <div className="blog-preview" key={e.id}>
+          <h2>{e.title}</h2>
+          <p>Written byt {e.author}</p>
+        </div>
+      ))}
     </div>
   );
-}
-
-
+};
 
 export default Home;
