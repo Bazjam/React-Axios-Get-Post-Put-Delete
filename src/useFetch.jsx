a62e98049135a0d404481216ecd1956b577ff04a
Bazjam <bonusystem@gmail.com>
2023-03-06 13:40:50 +0200

    Router Route Switch Link

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -2,30 +2,30 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
-
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    axios
-      .get(url)
-      
-      .then((d) => {
-        const data = d.data;
-        setData(data);
-        setLoading(false);
-        setError(false)
-      })
-      
-      .catch(()=> {
-        setError(true)
-        setLoading(false);
-        console.log(error);
-    })
-  }, [url]);
+    
+      axios
+        .get(url)
 
-  return {data, loading, error}
-}
+        .then((d) => {
+          const data = d.data;
+          setData(data);
+          setLoading(false);
+          setError(false);
+        })
+
+        .catch(() => {
+          setError(true);
+          setLoading(false);
+          console.log(error);
+        });
+        
+    }, [url]);
+  return { data, loading, error };
+};
 
 export default useFetch;
